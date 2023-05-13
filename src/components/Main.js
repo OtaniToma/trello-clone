931c5d2557fdddd746c7f6b6d1345103c6aff422
OtaniToma <otani.toma@gmail.com>
2023-05-13 20:33:16 +0900

    add react-beautiful-dnd

---
+++ src/components/Main.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import dummyData from "../dummyData";
+
+const Main = () => {
+  const [data, setData] = useState(dummyData);
+
+  return (
+    <DragDropContext>
+      <div className="trello">
+        {data.map((section) => (
+          <Droppable key={section.id} droppableId={section.id}>
+            {(provided) => (
+              <div
+                className="trello-section"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <div className="trello-section-title">{section.title}</div>
+                <div className="trello-section-content">
+                  {section.tasks.map((task, index) => (
+                    <Draggable
+                      draggableId={task.id}
+                      index={index}
+                      key={task.id}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            ...provided.draggableProps.style,
+                            opacity: snapshot.isDragging ? "0.5" : "1",
+                          }}
+                        >
+                          <Card></Card>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  );
+};
+
+export default Main;
